e034fae8703e8f2493eb3ba54c2a9e7ef204fbd9
Shadow-nx <32244138+Shadow-nx@users.noreply.github.com>
2017-11-08 19:58:17 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,28 +11,25 @@ struct Node {
 };
 void show_list(Node *first) {
 	Node *curr=first;
-	if(curr != nullptr) {
-		do {
-			cout<<curr->data;
-			if(curr->next!=nullptr)
-				cout<<" -> ";
-			curr=curr->next;
-		} while (curr!=nullptr);
-		cout<<endl;
-	} else
-		cerr<<"list is empty"<<endl;
+	do {
+		cout<<curr->data;
+		if(curr->next!=nullptr)
+			cout<<" -> ";
+		curr=curr->next;
+	} while (curr!=nullptr);
+	cout<<endl;
 }
-void check_numbers(const char *argv,int *&numbers_array,int *numbers_count) {
+void check_numbers(const char *argv,int *&numbers_array,int &numbers_count) {
 	for(int i=0; i<strlen(argv); i++) {
 		if(argv[i]>='0'&& argv[i]<='9')
 			;
-		else (*numbers_count)++;
+		else numbers_count++;
 	}
-	(*numbers_count)++;
+	numbers_count++;
 
-	numbers_array=new int[(*numbers_count)];
+	numbers_array=new int[numbers_count];
 
-	for(int i=0; i<(*numbers_count); i++) {
+	for(int i=0; i<numbers_count; i++) {
 		numbers_array[i]=0;
 	}
 	int y=0;
@@ -45,27 +42,26 @@ void fill_array(int numbers_array[],int argc,char **argv) {
 	for(int i=0; i<argc-1; i++)
 		numbers_array[i]=atoi(argv[i+1]);
 }
-void fill_list(int numbers_array[],int numbers_count,Node **firstt,Node **last) {
+void fill_list(int numbers_array[],int numbers_count,Node **first,Node **last) {
 	for(int i=0; i<numbers_count; i++) {
 		Node *node=new Node {numbers_array[i],nullptr};
 		if((*last) == nullptr) {
 			(*last)=node;
-			(*firstt)=(*last);
+			(*first)=(*last);
 			continue;
 		}
 		(*last)->next = node;
 		(*last) = node;
 	}
 }
-void addlist(Node **first,Node **last) {
+void add_list(Node **first,Node **last) {
 	int *numbers_array=nullptr;
 	int numbers_count=0;
 	string str;
 	cout<<"Please enter elements"<<endl;
 	cin.ignore(numeric_limits<streamsize>::max(), '\n');
 	getline(cin,str);
-	const char *c_str=str.c_str();
-	check_numbers(c_str,numbers_array,&numbers_count);
+	check_numbers(str.c_str(),numbers_array,numbers_count);
 	fill_list(numbers_array,numbers_count,first,last);
 }
 void print_menu() {
@@ -83,7 +79,7 @@ int main(int argc,char *argv[]) {
 	int *numbers_array=nullptr;
 	int numbers_count=0;
 	if(argc == 2) {
-		check_numbers(argv[1],numbers_array,&numbers_count);
+		check_numbers(argv[1],numbers_array,numbers_count);
 	} else if(argc>2) {
 		numbers_count=argc-1;
 		numbers_array=new int[numbers_count];
@@ -99,10 +95,13 @@ int main(int argc,char *argv[]) {
 		cin>>choice;
 		switch(choice) {
 			case 1:
-				show_list(first);
+				if(first != nullptr)
+					show_list(first);
+				else
+					cerr<<"list is empty"<<endl;
 				break;
 			case 2:
-				addlist(&first,&last);
+				add_list(&first,&last);
 				break;
 			case 3:
 				break;
